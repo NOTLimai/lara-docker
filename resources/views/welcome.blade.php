b1c03baa2d8fb93ac31807a1b0876b2a36c08684
NOTLimai <truant.web.dev@gmail.com>
2024-03-28 22:30:54 +0100

    add laravel sail with docker & install tailwindCss on the project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Title</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+<h1 class="text-3xl underline">
+    Hello world!
+</h1>
+</body>
+</html>
