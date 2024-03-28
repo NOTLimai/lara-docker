5c507d0c89079644c309c24ec4cb7d3934e1dfa0
NOTLimai <truant.web.dev@gmail.com>
2024-03-28 23:27:13 +0100

    add link on home page & text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,13 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Title</title>
+    <title>Lara-Docker</title>
     @vite('resources/css/app.css')
 </head>
 <body>
-<h1 class="text-3xl underline">
-    Hello world!
-</h1>
+    <div class="flex justify-center items-center mx-auto h-screen flex-col">
+        <h1 class="text-3xl">Welcome your Lara-Docker Clone</h1>
+        <a class="bg-red-600 text-white px-4 py-2 rounded-lg mt-4" href="https://github.com/NOTLimai/lara-docker" target="_blank">Github</a>
+    </div>
 </body>
 </html>
